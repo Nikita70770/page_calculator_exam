e630c49cee3965d5d41c2c6ff5f4cfbd9449f1b2
Nikita70770 <filippovni1999@gmail.com>
2024-01-30 16:43:29 +0300

    Save project

--- js/script.js
+++ js/script.js
@@ -18,103 +18,70 @@ const filters_aggressiveness = document.querySelectorAll('.aggressiveness > *');
 //
 const building_structures = document.querySelectorAll('.building_structures > * input[type="checkbox"]');
 const cb_select_all_1 = building_structures[0];
-
 //
 const measuring_works = document.querySelectorAll('.measuring_works > * input[type="checkbox"]');
 const cb_select_all_2 = measuring_works[0];
-
 //
 const engineering_systems = document.querySelectorAll('.engineering_systems > * input[type=checkbox]');
 const cb_select_all_3 = engineering_systems[0];
 
+// Кнопка 'Скачать смету'
 const btn_download_estimate = document.querySelector('.btn_download_estimate');
 const sub_menu = document.querySelector('.sub_menu');
 
 // Добавление обработчика события 'change'
 filters_appointment.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_appointment) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_appointment, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_design_scheme.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_design_scheme) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_design_scheme, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_complexity_obj.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_complexity_obj) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_complexity_obj, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_complexity_survey.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_complexity_survey) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_complexity_survey, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_complexity_works.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_complexity_works) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_complexity_works, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_original_drawings.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_original_drawings) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_original_drawings, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_seismicity_area.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_seismicity_area) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_seismicity_area, e);
     });
 });
 
 // Добавление обработчика события 'change'
 filters_aggressiveness.forEach(function (item_list) {
     item_list.querySelector('input[type=checkbox]').addEventListener('change', function (e) {
-        for (item of filters_aggressiveness) {
-            item.classList.remove('active');
-        }
-        active_elem = e.target.parentNode;
-        active_elem.classList.add('active');
+        checkbox_change(filters_aggressiveness, e);
     });
 });
 
@@ -156,6 +123,39 @@ cb_select_all_3.addEventListener('change', () => {
         });
     }
 });
+
+building_structures.forEach(function (checkbox) {
+    if (checkbox != cb_select_all_1) {
+        checkbox.addEventListener('change', function (e) {
+            cb_select_all_1.checked = false;
+        });
+    }
+});
+
+measuring_works.forEach(function (checkbox) {
+    if (checkbox != cb_select_all_2) {
+        checkbox.addEventListener('change', function (e) {
+            cb_select_all_2.checked = false;
+        });
+    }
+});
+
+engineering_systems.forEach(function (checkbox) {
+    if (checkbox != cb_select_all_3) {
+        checkbox.addEventListener('change', function (e) {
+            cb_select_all_3.checked = false;
+        });
+    }
+});
+
+// Обработчик события 'change'
+function checkbox_change(filters, checkbox) {
+    for (item of filters) {
+        item.classList.remove('active');
+    }
+    active_elem = checkbox.target.parentNode;
+    active_elem.classList.add('active');
+}
 
 btn_download_estimate.onclick = () => {
     sub_menu.classList.toggle('show');
